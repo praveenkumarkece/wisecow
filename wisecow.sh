e170785c918be641f96747319a6232768d1a0d51
Praveen Kumar K <121642221+praveenkumarkece@users.noreply.github.com>
2023-06-25 02:30:45 +0530

    Update wisecow.sh
    updated

--- wisecow.sh
+++ wisecow.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
-# Set the PATH environment variable
-export PATH="/usr/games:${PATH}"
+
 SRVPORT=4499
 RSPFILE=response
 
@@ -8,40 +7,39 @@ rm -f $RSPFILE
 mkfifo $RSPFILE
 
 get_api() {
-	read line
-	echo $line
+  read line
+  echo $line
 }
 
 handleRequest() {
-    # 1) Process the request
-	get_api
-	mod=$(fortune)
+  # 1) Process the request
+  get_api
+  mod=$(fortune)
 
-cat <<EOF > $RSPFILE
+  cat <<EOF > $RSPFILE
 HTTP/1.1 200
 
-
-<pre>$(cowsay $mod)</pre>
+<pre>\`cowsay \$mod\`</pre>
 EOF
 }
 
 prerequisites() {
-	command -v cowsay >/dev/null 2>&1 &&
-	command -v fortune >/dev/null 2>&1 || 
-		{ 
-			echo "Install prerequisites."
-			exit 1
-		}
+  command -v cowsay >/dev/null 2>&1 &&
+    command -v fortune >/dev/null 2>&1 ||
+    {
+      echo "Install prerequisites."
+      exit 1
+    }
 }
 
 main() {
-	prerequisites
-	echo "Wisdom served on port=$SRVPORT..."
+  prerequisites
+  echo "Wisdom served on port=$SRVPORT..."
 
-	while true; do
-		cat $RSPFILE | nc -lN $SRVPORT | handleRequest
-		sleep 0.01
-	done
+  while [ 1 ]; do
+    cat $RSPFILE | nc -lN $SRVPORT | handleRequest
+    sleep 0.01
+  done
 }
 
 main
